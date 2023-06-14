582202a495befa10266b6b28badcbdd8144b7f82
kulotsystems <kulotsystems@gmail.com>
2023-06-14 23:23:49 +0800

    Update config.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'io.ionic.starter',
+    appId: 'tech.kulotsystems.ron',
     appName: 'ron',
     webDir: 'dist',
     server: {
